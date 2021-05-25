7f9f7e0aaac3dd62a2b8804c4a8e37edf5cf69ae
faycal <flbouras@hotmail.com>
2021-05-25 15:55:32 +0200

    ajout create update delete vendeur

--- cakeandcollect_backend/src/controllers/vendeur.controller.js
+++ cakeandcollect_backend/src/controllers/vendeur.controller.js
@@ -32,4 +32,71 @@ module.exports = {
             res.status(400).send(e);
         }
     },
+
+    async createVendeur(req, res) {
+        try {
+            const vendeurCreated = await vendeur.create({
+                nom: req.body.nom,
+                prenom: req.body.prenom,
+                siret: req.body.siret,
+                mdp: req.body.mdp,
+                particulier: req.body.particulier,
+                actif: req.body.actif,
+                rue: req.body.rue,
+                code_postal: req.body.code_postal,
+                ville: req.body.ville,
+                code_promo: req.body.code_promo,
+            });
+            res.status(201).send(vendeurCreated)
+        }
+        catch (e) {
+            console.log(e);
+            res.status(400).send(e);
+        }
+    },
+
+    async updateVendeur(req, res) {
+        const id = req.params.id;
+        try {
+            const oneVendeur = await vendeur.findOne(({
+                where: { id_vendeur: id }
+            }))
+            if (oneVendeur) {
+                const updatedVendeur = await oneVendeur.update(req.body, {
+                    where: { id_vendeur: id }
+                })
+
+                res.status(201).send(updatedVendeur);
+            }
+            else {
+                res.status(404).send("Vendeur Not Found");
+            }
+        }
+        catch (e) {
+            console.log(e);
+            res.status(400).send(e);
+        } 
+    },
+
+    async deleteVendeur(req, res) {
+        const id = req.params.id;
+        try {
+
+            const deletedVendeur = await vendeur.findOne(({
+                where: { id_vendeur: id }
+            }))
+            if (deletedVendeur) {
+                deletedVendeur.destroy();
+                res.status(201).send("Deleted");
+            }
+            else {
+                res.status(404).send("Vendeur Not Found");
+            }
+
+        }
+        catch (e) {
+            console.log(e);
+            res.status(400).send(e);
+        }
+    },
 }
--- cakeandcollect_backend/src/routes/vendeur.routes.js
+++ cakeandcollect_backend/src/routes/vendeur.routes.js
@@ -6,8 +6,8 @@ module.exports = function (app) {
 
     
     app.get("/api/vendeurs/:id", controller.getVendeurById); // http://localhost:8080/api/vendeurs/1
-    //app.post("/api/vendeurs", controller.createVendeur); // http://localhost:8080/api/vendeurs
-    //app.put("/api/vendeurs/:id", controller.updateVendeur); // http://localhost:8080/api/vendeurs/1
-    //app.delete("/api/vendeurs/:id", controller.deleteVendeur); // http://localhost:8080/api/vendeurs/1
+    app.post("/api/vendeurs", controller.createVendeur); // http://localhost:8080/api/vendeurs
+    app.put("/api/vendeurs/:id", controller.updateVendeur); // http://localhost:8080/api/vendeurs/1
+    app.delete("/api/vendeurs/:id", controller.deleteVendeur); // http://localhost:8080/api/vendeurs/1
 
 }
