4bda2ebf032c36998de866730cd33ed3b5f2bb9d
faycal <flbouras@hotmail.com>
2021-05-27 14:28:12 +0200

    [BF] getallcategories

--- ionic-cakeandcollect/src/app/app.module.ts
+++ ionic-cakeandcollect/src/app/app.module.ts
@@ -8,11 +8,24 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- ionic-cakeandcollect/src/app/pages/accueil/accueil.module.ts
+++ ionic-cakeandcollect/src/app/pages/accueil/accueil.module.ts
@@ -1,18 +1,15 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { AccueilPageRoutingModule } from './accueil-routing.module';
-
 import { AccueilPage } from './accueil.page';
 
 @NgModule({
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
     AccueilPageRoutingModule
   ],
   declarations: [AccueilPage]
