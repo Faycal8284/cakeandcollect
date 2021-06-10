e7e75fc87dab576c8674d60f986716464e7977d1
Khadidja IKHLEF <khadidja.ikhlef.mobile@gmail.com>
2021-06-10 15:26:17 +0200

    [KI] Add Register Vendeur

--- ionic-cakeandcollect/src/app/pages/vendeur/register-vendeur/register-vendeur.page.ts
+++ ionic-cakeandcollect/src/app/pages/vendeur/register-vendeur/register-vendeur.page.ts
@@ -1,4 +1,9 @@
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.vendeur.service';
+import { TokenStorageService } from 'src/app/shared/token-storage.service';
 
 @Component({
   selector: 'app-register-vendeur',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterVendeurPage implements OnInit {
 
-  constructor() { }
+  id: number;
+
+  register: any = {
+    nom: null,
+    prenom: null,
+    siret: null,
+    email: null,
+    mdp: null,
+    img: null,
+    categorie: null,
+    note: null,
+    code_promo: null,
+    partticulier: null,
+    tel: null,
+    descriptions: null,
+    actif: null,
+    rue: null,
+    code_postal: null,
+    ville: null
+  };
+
+  isLoggedIn = false;
+  isLoginFailed = false;
+
+  errorMessage = '';
+
+  constructor(private router: Router, private authService: AuthService,
+              private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+  }
+
+  isRegistred() {
+    const { nom, prenom, siret, email, mdp, img, categorie, note, code_promo, partticulier, tel, descriptions, actif, rue, code_postal, ville } = this.register;
+    this.authService.register(nom, prenom, siret, email, mdp, img, categorie, note, code_promo, partticulier, tel, descriptions, actif, rue, code_postal, ville )
+    .subscribe(
+      data => {
+        //this.tokenStorage.saveToken(data.accessToken);
+        //this.tokenStorage.saveUser(data);
+        console.log(data);
+
+        //this.isLoggedIn = true;
+        //this.isLoginFailed = false;
+        //this.router.navigateByUrl('/espace-vendeur'); // navigate vers tableau de bord vendeur
+        //this.router.navigate(['espace-vendeur', data.id]);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
   }
 
 }
